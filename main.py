7d2f1d1e689f019e753f406fec182d3044f14b56
pycharm <thomasrtmueller@gmail.com>
2023-06-09 18:55:01 +0300

    enable all functions

--- main.py
+++ main.py
@@ -64,15 +64,14 @@ def post_news():
         sleep(sleep_time)
 
 
-# if __name__ == "__main__":
-#     p1 = Process(target=post_news)
-#     p1.start()
-#     p2 = Process(target=reply_mentions)
-#     p2.start()
-#     p3 = Process(target=reply_tweet)
-#     p3.start()
-#     p1.join()
-#     p2.join()
-#     p3.join()
+if __name__ == "__main__":
+    p1 = Process(target=post_news)
+    p1.start()
+    p2 = Process(target=reply_mentions)
+    p2.start()
+    p3 = Process(target=reply_tweet)
+    p3.start()
+    p1.join()
+    p2.join()
+    p3.join()
 
-post_news()
