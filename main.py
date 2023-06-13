bdee6a46b78a6f37db94b8060a577d552b18e14b
pycharm <thomasrtmueller@gmail.com>
2023-06-13 18:13:35 +0300

    fix bug

--- main.py
+++ main.py
@@ -64,18 +64,19 @@ def post_news():
         logger.info(f"Post new tweets complete sleeping for minutes: {int(sleep_time / 60)}")
         sleep(sleep_time)
 
-#
-# if __name__ == "__main__":
-#     p1 = Process(target=post_news)
-#     p1.start()
-#     p2 = Process(target=reply_mentions)
-#     p2.start()
-#     p3 = Process(target=reply_tweet)
-#     p3.start()
-#     p1.join()
-#     p2.join()
-#     p3.join()
 
+if __name__ == "__main__":
+    p1 = Process(target=post_news)
+    p1.start()
+    p2 = Process(target=reply_mentions)
+    p2.start()
+    p3 = Process(target=reply_tweet)
+    p3.start()
+    p1.join()
+    p2.join()
+    p3.join()
+
+# For testing
 # reply_tweet()
 # reply_mentions()
-post_news()
+# post_news()
